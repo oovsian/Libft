a955e4597e40bd368b11f53ca0008747a5feef67
oovsian <166440967+oovsian@users.noreply.github.com>
2025-03-19 22:12:11 +0100

    Update ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -30,4 +30,10 @@ void	ft_putendl_fd(char *s, int fd)
 		write(fd, "\n", 1);
 	}
 }
-//Envía la string ’s’ al file descriptor dado, seguido de un salto de línea
+// Envía al output la cadena 's' del file descriptor dado, seguida de un salto de línea
+// O solo así:
+/* void	ft_putendl_fd(char const *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}*/
